6f2cb57a2c69b9394e6cf3d3d6efef9ccfec7653
Fran.js <59714199+FranReza@users.noreply.github.com>
2023-06-02 17:47:33 -0600

    YA INSERTA EN LA BASE DE DATOS
    ASKDLASDASLKDNASLDKJSAN

--- api-cotizador/controllers/cotizacionController.js
+++ api-cotizador/controllers/cotizacionController.js
@@ -3,139 +3,222 @@ const dbconfig = require('../database/config');
 const consultas = require('../database/consultas');
 
 const grabarCotizacionMsp = (req, res) => {
+  const { CLIENTE, FECHAS, ARTICULOS, METODO_PAGO, VENDEDOR_ID, SUBTOTAL_DOC, IMPUESTOS_TOTAL_DOC, TOTAL_GENERAL } = req.body;
 
-    const { CLIENTE, FECHAS, ARTICULOS, METODO_PAGO, VENDEDOR_ID, SUBTOTAL_DOC, IMPUESTOS_TOTAL_DOC, TOTAL_GENERAL } = req.body;
+  Firebird.attach(dbconfig, function (err, db) {
+    if (err) {
+      console.log('Error al tratar de conectar a la base de datos microsip...');
+      return;
+    }
 
-    Firebird.attach(dbconfig, function (err, db) {
+    db.transaction(Firebird.ISOLATION_READ_COMMITED, function (err, transaction) {
+      if (err) {
+        console.log('Error al iniciar la transacción');
+        db.detach();
+        return;
+      }
 
+      transaction.query(consultas.generadorFolio, function (err, result) {
         if (err) {
-            console.log('error al tratar de consultar a la base de datos microsip...');
+          console.log('Error al tratar de consultar el folio');
+          transaction.rollback();
+          res.status(301).send({ mensaje: 'Error al tratar de generar el folio: ' + err });
+          db.detach();
+          return;
         }
 
-        db.transaction(Firebird.ISOLATION_READ_COMMITED, function (err, transaction) {
-            if (err) {
-                console.log('error al iniciar la transacción');
-                return;
+        const genFolio = result;
+        const folioCompleto = `@${genFolio.FOLIO_TEMP.toString().padStart(8, '0')}`;
+
+        const fechaHora = new Date();
+        const hora = fechaHora.toTimeString().split(' ')[0];
+        const fechaHoraDoc = fechaHora.toISOString().replace('T', ' ').split('.')[0];
+
+        const datos_doctos_ve = [
+          -1,
+          'C',
+          'N',
+          1944,
+          folioCompleto,
+          FECHAS.fecha_doc,
+          hora,
+          null,
+          CLIENTE.CLIENTE_ID,
+          CLIENTE.DIR_CLI_ID,
+          CLIENTE.DIR_CLI_ID,
+          null,
+          null,
+          1,
+          0,
+          'P',
+          0,
+          0,
+          'P',
+          'S',
+          FECHAS.fecha_vencimiento,
+          null,
+          null,
+          null,
+          null,
+          null,
+          SUBTOTAL_DOC,
+          0,
+          0,
+          IMPUESTOS_TOTAL_DOC,
+          0,
+          0,
+          0,
+          'N',
+          'N',
+          'N',
+          'VE',
+          CLIENTE.COND_PAGO_ID,
+          null,
+          0,
+          VENDEDOR_ID,
+          0,
+          null,
+          0,
+          METODO_PAGO,
+          null,
+          null,
+          'SYSDBA',
+          'N',
+          null,
+          'N',
+          null,
+          null,
+          'N',
+          null,
+          null,
+          'N',
+          null,
+          fechaHoraDoc,
+          'SYSDBA',
+          null,
+          fechaHoraDoc,
+          'S',
+          null,
+          null,
+          null,
+          null,
+        ];
+
+        console.log('folio generado: '+ folioCompleto);
+
+        transaction.query(consultas.insertDoctoVe, datos_doctos_ve, function (err, result) {
+          if (err) {
+            console.log('Error al tratar de insertar en la primera tabla');
+            transaction.rollback();
+            res.status(301).send({ mensaje: 'Error al tratar de insertar en la primera tabla: ' + err });
+            db.detach();
+            return;
+          }
+
+          const idDoctosVe = result.DOCTO_VE_ID;
+          console.log(idDoctosVe);
+          let posicion = 1;
+
+          const insertions = ARTICULOS.map((articulo) => {
+            if (articulo.UNIDAD_VENTA === 'Metro cuadrado') {
+              const mcuadrado = articulo.MCUADRADO;
+              const mcuadradoFloat = +parseFloat(mcuadrado);
+              const calc_dscto = +parseFloat((((articulo.PRECIO * mcuadradoFloat) * articulo.DESCTO) / 100)).toFixed(2);
+
+              const datos_doctos_ve_det_mc = [
+                -1,
+                idDoctosVe,
+                articulo.CLAVE_ARTICULO,
+                articulo.ARTICULO_ID,
+                mcuadradoFloat,
+                0,
+                0,
+                0,
+                articulo.PRECIO,
+                articulo.DESCTO,
+                calc_dscto,
+                articulo.DESCTO,
+                0,
+                0,
+                0,
+                parseFloat(articulo.TOTAL),
+                0,
+                'N',
+                articulo.NOTAS !== '' ? articulo.NOTAS : null,
+                posicion,
+              ];
+
+              console.log('Se inserta en la tabla con mcuadrado');
+              console.log(datos_doctos_ve_det_mc);
+
+              return { insert: datos_doctos_ve_det_mc };
+            } else {
+              const calc_dscto = +parseFloat((((articulo.PRECIO * articulo.CANTIDAD) * articulo.DESCTO) / 100)).toFixed(2);
+
+              const datos_doctos_ve_det_pz = [
+                -1,
+                idDoctosVe,
+                articulo.CLAVE_ARTICULO,
+                articulo.ARTICULO_ID,
+                parseInt(articulo.CANTIDAD),
+                0,
+                0,
+                0,
+                articulo.PRECIO,
+                articulo.DESCTO,
+                calc_dscto,
+                articulo.DESCTO,
+                0,
+                0,
+                0,
+                parseFloat(articulo.TOTAL),
+                0,
+                'N',
+                articulo.NOTAS !== '' ? articulo.NOTAS : null,
+                posicion,
+              ];
+
+              console.log('Se inserta en la tabla con cantidad');
+              console.log(datos_doctos_ve_det_pz);
+
+              return { insert: datos_doctos_ve_det_pz };
             }
+          });
 
-            //generamos el folio
-            transaction.query(consultas.generadorFolio, function (err, result) {
+          let insertedCount = 0;
 
-                if (err) {
-                    console.log('error al tratar de consultar el folio');
+          insertions.forEach((insert) => {
+            transaction.query(consultas.insertDoctoVeDet, insert.insert, function (err, result) {
+              if (err) {
+                console.log('Error al insertar en la segunda tabla');
+                transaction.rollback();
+                res.status(301).send({ mensaje: 'Error al tratar de insertar en la segunda tabla: ' + err });
+                db.detach();
+                return;
+              }
+
+              insertedCount++;
+
+              if (insertedCount === insertions.length) {
+                transaction.commit(function (err) {
+                  if (err) {
+                    console.log('Error al confirmar la transacción');
                     transaction.rollback();
-                    res.status(301).send({ mensaje: 'error al tratar de generar el folio : ' + err });
+                    res.status(301).send({ mensaje: 'Error al confirmar la transacción: ' + err });
+                    db.detach();
                     return;
-                }
+                  }
 
-                //generamos el folio del documento para iniciar
-                const genFolio = result;
-                const folioCompleto = `@${genFolio.FOLIO_TEMP.toString().padStart(8, '0')}`;
-
-                const fechaHora = new Date(); // Obtén la fecha y hora actual
-
-                const fecha = fechaHora.toISOString().split('T')[0]; // Obtiene la fecha en formato "YYYY-MM-DD"
-                const hora = fechaHora.toTimeString().split(' ')[0]; // Obtiene la hora en formato "HH:MM:SS"
-
-                const fechaHoraDoc = fechaHora.toISOString().replace('T', ' ').split('.')[0];
-
-                const datos_doctos_ve = [
-                    -1,
-                    'C',
-                    'N',
-                    1944,
-                    folioCompleto,
-                    FECHAS.fecha_doc,
-                    hora,
-                    null,
-                    CLIENTE.CLIENTE_ID,
-                    CLIENTE.DIR_CLI_ID,
-                    CLIENTE.DIR_CLI_ID,
-                    null,
-                    null,
-                    1,
-                    0,
-                    'P',
-                    0,  //DSCTO_PCTJE
-                    0, //DSCTO_IMPORTE
-                    'P', //STATUS,
-                    'S', //APLICADO
-                    FECHAS.fecha_vencimiento,
-                    null,
-                    null,
-                    null, //folio recibo de mercancia
-                    null,
-                    null,
-                    SUBTOTAL_DOC, //IMPORTE NETO
-                    0,
-                    0,
-                    IMPUESTOS_TOTAL_DOC,
-                    0,
-                    0,
-                    0,
-                    'N', //FORMA EMITIDA
-                    'N',
-                    'N',
-                    'VE',
-                    CLIENTE.COND_PAGO_ID,
-                    null,
-                    0,
-                    VENDEDOR_ID,
-                    0,  //porcentaje de comision de el vendedor id, nose que podriamos poner aqui...
-                    null,
-                    0,
-                    METODO_PAGO,
-                    null,
-                    null,
-                    'SYSDBA',
-                    'N',
-                    null,
-                    'N',
-                    null,
-                    null,
-                    'N',
-                    null,
-                    null,
-                    'N', //cfdi certificado,
-                    null,
-                    fechaHoraDoc,
-                    'SYSDBA',
-                    null,
-                    fechaHoraDoc,
-                    'S',
-                    null,
-                    null,
-                    null,
-                    null,
-                ];
-
-                transaction.query(consultas.insertDoctoVe, datos_doctos_ve, function (err, result) {
-                    if (err) {
-                        console.log('error al tratar de consultar el folio');
-                        transaction.rollback();
-                        res.status(301).send({ mensaje: 'error al tratar de generar el folio : ' + err });
-                        return;
-                    }
-
-                    const idInsertado = result?.insertId;
-                    console.log('Registro insertado con ID:', idInsertado);
-
-                    transaction.commit(function (err) {
-                        if (err) {
-                            console.log('Error al realizar el commit:', err);
-                            transaction.rollback();
-                            res.status(301).send({ mensaje: 'Error al realizar el commit: ' + err });
-                            return;
-                        }
-
-                        console.log('Transacción confirmada');
-                        res.status(200).send({ mensaje: 'Registro insertado con ID: ' + idInsertado });
-                        db.detach();
-                    });
+                  console.log('Datos insertados correctamente en ambas tablas.');
+                  db.detach();
                 });
+              }
             });
+          });
         });
+      });
     });
+  });
 };
 
-module.exports = { grabarCotizacionMsp }
+module.exports = { grabarCotizacionMsp };
